12ea9e711a2447bc14bb5ee02b52d58ffc7159b4
Eckid <851976472@qq.com>
2016-01-05 23:34:14 +0800

    pj-questionaire-demo

---
+++ example/pj-questionnaire/js/main.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Administrator on 2016/1/4 0004.
+ */
+
+(function() {
+    var curShow = 0;
+    var dtlMenuList = document.getElementById('dtl-menu-adult').getElementsByTagName('li');
+    var dtlMenuCont = document.getElementById('detail-cont-hide-adult').getElementsByTagName('table');
+
+    for(var i = 1;i<dtlMenuCont.length;i++){
+        dtlMenuCont[i].style.display = 'none';
+    }
+
+    var showMenu = function(i) {
+        dtlMenuList[i].onmouseover = function () {
+            dtlMenuCont[curShow].style.display = 'none';
+            dtlMenuList[curShow].removeAttribute("id","dtl-menu-selected")
+            curShow = i
+            dtlMenuCont[curShow].style.display = 'table';
+            dtlMenuList[curShow].setAttribute("id","dtl-menu-selected")
+
+        }
+    }
+    for(var i=0;i<dtlMenuList.length;i++){
+        showMenu(i);
+    }
+
+}())
---
+++ example/pj-questionnaire/js/test.js
@@ -0,0 +1,64 @@
+/**
+ * Created by Administrator on 2016/1/5 0005.
+ */
+
+var content = getElementsClass('content');
+var testIntro = getElementsClass('test-intro');
+var testContent = getElementsClass('test-cont');
+var startBtn = document.getElementById('startTest');
+var question = getElementsClass('question');
+
+(function () {
+    content[0].appendChild(testIntro[0]);
+    testContent[0].style.display = 'none';
+
+    startBtn.onclick = function(){
+        content[0].removeChild(testIntro[0]);
+        testContent[0].style.display = 'block';
+        showTest(0);
+    }
+
+}())
+
+
+function setProgress(i){
+        var cur = i+1;
+        var width = cur/question.length*100;
+        document.getElementById('progress-bar').style.width = width+'%';
+        document.getElementById('progress-value').innerHTML = cur+"/"+question.length;
+
+}
+
+function showTest(i){
+    var selectList = question[i].getElementsByTagName('li');
+    setProgress(i);
+    question[i].style.display = 'block';
+    var next = function(){
+        question[i].style.display='none';
+            showTest(i+1);
+    }
+    if(i<question.length-1) {
+        for (var n = 0; n < selectList.length; n++) {
+            selectList[n].onclick = function () {
+                next()
+            }
+        }
+
+    }
+}
+
+function getElementsClass(classnames) {
+    var classobj = new Array();
+    var classint = 0;
+    var tags = document.getElementsByTagName("*");
+    for (var i in tags) {
+        if (tags[i].nodeType == 1) {
+            if (tags[i].getAttribute("class") == classnames)
+            {
+                classobj[classint] = tags[i];
+                classint++;
+            }
+        }
+    }
+    return classobj;
+}
