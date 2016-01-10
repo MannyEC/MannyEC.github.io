da8fa49ed1a90420e0e9ec8616b9bce6f11b65a3
Eckid <851976472@qq.com>
2016-01-11 04:11:13 +0800

    pj

---
+++ example/pj-questionnaire/xml/delete.py
@@ -0,0 +1,28 @@
+url = 'child05.xml'
+f = open(url,'r')
+doc = f.read()
+a = str()
+index = 0
+for ch in doc:
+    if(index==len(doc)-1):
+        a=a+doc[index]
+        break
+    if((doc[index]=='\\')&(doc[index+1]=='n')):
+       index+=1
+    else:
+        if ((doc[index-1]=='\\')&(doc[index]=='n')):
+            index+=1
+        else:
+           a = a+doc[index]
+           index+=1
+
+f.close()
+
+
+
+f2 = open(url,'w')
+
+f2.write(a)
+
+f2.close()
+
